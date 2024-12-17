eb77d21576ebc80278407bb3508bad75535ae8f1
Nandanpaul <nandanpaul44@gmail.com>
2024-12-17 22:08:56 +0600

    Add files via upload

---
+++ Week 8/Day 1/Day 2/Battle_for_Survive.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        ll sum=0;
+        for(ll i=1;i<=n;i++)
+        {
+            int x;
+            cin>>x;
+            if(i<=n-2)
+            {
+                sum+=x;
+            }
+            else if(i==n-1)
+            {
+                sum-=x;
+            }
+            else
+            {
+                sum+=x;
+            }
+        }
+        cout<<sum<<endl;
+    }
+}
