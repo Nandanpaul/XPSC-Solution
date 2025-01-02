6968db6d148c476e092b3cc05d2ab2a1f59c09e7
Nandanpaul <nandanpaul44@gmail.com>
2025-01-02 13:44:29 +0600

    Add files via upload

---
+++ Week 10/Day 3/Large_Factorial.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD=1e9+7;
+int main()  
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int ans=1;
+        for(int i=1;i<=n;i++)
+        {
+            ans=(1LL*(ans%MOD)*(i%MOD))%MOD;
+        }
+        cout<<ans<<endl;
+    }
+}
